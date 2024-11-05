ad61b9b5cdb84aa8f836dd5dc7c7006674754897
EthanMoveo <ethanf@moveo.co.il>
2024-11-05 16:20:04 +0200

    add db configuration

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const connectToDB = require('./config/db');
 const PORT = 3000;
+
+connectToDB();
 
 
 app.get('/', (req, res) => {
     res.status(200);
-    res.send("Server is live.")
+    res.send("Server is live.");
 })
 
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+    console.log(`Server is listening on port ${PORT}`);
 })
---
+++ config/db.js
@@ -0,0 +1,14 @@
+// config/db.js
+const mongoose = require('mongoose');
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/epicure');
+    console.log('Connection to MongoDB was successful.');
+  } catch (error) {
+    console.error('An error occurred while trying to connect to MongoDB:', error);
+    process.exit(1); 
+  }
+};
+
+module.exports = connectToDB;
