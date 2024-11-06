2b38589bdceecec8c3a8d519e824919555de17d2
EthanMoveo <ethanf@moveo.co.il>
2024-11-06 09:52:43 +0200

    implement login endpoint without token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -22,4 +22,24 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: 'This username does not exist in the system.' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'The password is incorrect.' });
+    }
+
+    res.status(200).json({ message: 'Login successful.' });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred:', error });
+  }
+};
+
+module.exports = { register, login };
