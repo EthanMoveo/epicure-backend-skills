508d19ff2e8cee8b064c3edd2919e8323c946c6e
EthanMoveo <ethanf@moveo.co.il>
2024-11-06 09:46:00 +0200

    add encryption to password when saving to db

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,20 +1,25 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt'); // Importer bcrypt
 
 const register = async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      const existingUser = await User.findOne({ username });
-      if (existingUser) {
-        return res.status(400).json({ message: 'Username already been taken.' });
-      }
-  
-      const user = new User({ username, password });
-      await user.save();
-      res.status(201).json({ message: 'User saved successfully in the database.' });
-    } catch (error) {
-      res.status(500).json({ message: 'An error occurred: ', error });
-    }
-  };
+  const { username, password } = req.body;
 
-module.exports = { register }
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already been taken.' });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    
+    res.status(201).json({ message: 'User saved successfully in the database.' });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred: ', error });
+  }
+};
+
+module.exports = { register };
